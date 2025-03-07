70c1ec78a1d65805133921e382fd7eb54e8fddac
MT <59849596+moomurrs@users.noreply.github.com>
2025-03-07 08:56:56 -0500

    add part 3

--- JS1/script.js
+++ JS1/script.js
@@ -3,13 +3,21 @@ function changeParagraph() {
     //console.log("yes");
 }
 
-function changeBackgroundColor(){
+function changeBackgroundColor() {
     let colors = ["blanchedalmond", "cadetblue", "darksalmon", "darkseagreen", "lightblue"];
     let new_color = colors[Math.floor(Math.random() * 4)];
     //console.log(new_color);
 
     document.getElementById("body").style.backgroundColor = new_color;
 }
+
+function showGreeting() {
+    let name = document.getElementById("inputText").value;
+    console.log(name);
+    document.getElementById("greeting").innerText = "Hello, " + name + "!";
+}
+
+document.getElementById("submitName").addEventListener("click", showGreeting);
 
 document.getElementById("messageButton").addEventListener("click", changeParagraph);
 document.getElementById("colorButton").addEventListener("click", changeBackgroundColor);
