ced310f0a135b84586642f091b0a4cec384bb0f6
MT <murun1life@gmail.com>
2025-03-09 16:16:34 -0400

    finish part 4, add comments

--- JS1/script.js
+++ JS1/script.js
@@ -1,8 +1,10 @@
-function changeParagraph() {
+// part 1
+function changeMessage() {
     document.getElementById("message").innerText = "Hello! You click the button!";
     //console.log("yes");
 }
 
+// part 2
 function changeBackgroundColor() {
     let colors = ["blanchedalmond", "cadetblue", "darksalmon", "darkseagreen", "lightblue"];
     let new_color = colors[Math.floor(Math.random() * 4)];
@@ -11,13 +13,34 @@ function changeBackgroundColor() {
     document.getElementById("body").style.backgroundColor = new_color;
 }
 
+// part 3
 function showGreeting() {
     let name = document.getElementById("inputText").value;
-    console.log(name);
+    //console.log(name);
     document.getElementById("greeting").innerText = "Hello, " + name + "!";
 }
 
-document.getElementById("submitName").addEventListener("click", showGreeting);
+// part 4
+function showHideText() {
+    let isShown = document.getElementById("hideButton").innerText === "Hide Text";
+    //console.log(isShown);
 
-document.getElementById("messageButton").addEventListener("click", changeParagraph);
+    if (isShown) {
+        document.getElementById("hiddenText").style.display = "none";
+        document.getElementById("hideButton").innerText = "Show Text";
+    } else {
+        document.getElementById("hiddenText").style.display = "block";
+        document.getElementById("hideButton").innerText = "Hide Text";
+    }
+
+    //console.log(document.getElementById("hideButton").innerText);
+}
+
+// part 1
+document.getElementById("messageButton").addEventListener("click", changeMessage);
+// part 2
+document.getElementById("submitName").addEventListener("click", showGreeting);
+// part 3
 document.getElementById("colorButton").addEventListener("click", changeBackgroundColor);
+// part 4
+document.getElementById("hideButton").addEventListener("click", showHideText);
