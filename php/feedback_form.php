f01615bf96558050c1c0f1e81cf688ba6b2f3726
MT <murun1life@gmail.com>
2025-04-09 21:52:02 -0400

    finish part 1

--- php/feedback_form.php
+++ php/feedback_form.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     var_dump($_POST);
 }
 
@@ -11,10 +11,37 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 <body>
 
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
+        <label for="name">Name: </label>
+        <input type="text" name="name"><br>
+
+        <label for="email">Email: </label>
+        <input type="text" name="email"><br>
+
+        Rating:
+        <input type="radio" id="rating1" name="rating" value="1">
+        <label for="rating1">1</label>
+
+        <input type="radio" id="rating2" name="rating" value="2">
+        <label for="rating2">2</label>
+
+        <input type="radio" id="rating3" name="rating" value="3">
+        <label for="rating3">3</label>
+
+        <input type="radio" id="rating4" name="rating" value="4">
+        <label for="rating4">4</label>
+
+        <input type="radio" id="rating3" name="rating" value="5">
+        <label for="rating5">5</label>
+
+        <br>
+
+        <textarea placeholder="Enter text..." id="textarea" name="textarea"></textarea>
+
+        <br>
+
+
         <input type="submit">
     </form>
 
